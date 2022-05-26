19d8d16a6f6235311aa30c3e68453c3e9416285b
Shiva Kumar <83355044+Shiva02Kumar@users.noreply.github.com>
2022-05-26 18:41:55 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,12 +4,14 @@ from bs4 import BeautifulSoup
 url = 'https://www.timeanddate.com/weather/'
 
 country = input("Enter Country : ")
-country = country.strip()
 state = input("Enter State/City : ")
+
+country = country.strip()
 state = state.strip()
 url = url + country.lower() + "/" + state.lower()
 
 r = requests.get(url)
+
 if not r:
     print("Country or State not found")
     exit(0)
@@ -19,7 +21,6 @@ soup = BeautifulSoup(htmlcontent,'html.parser')
 
 weather1 = soup.find(id = 'qlook')
 weather = weather1.find_all("div", class_="h2")
-
 predict = weather1.p
 predict1 = predict.find_next_sibling("p")
 pre = predict1.contents
